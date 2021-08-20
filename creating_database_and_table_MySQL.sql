36390e8fbe76b9768c83f8b3c5d7889b39f951ee
Fengxuan Zhang <41898293+Phoenix0609@users.noreply.github.com>
2021-08-20 16:50:35 +0100

    Update and rename aws db 2.sql to creating_database_and_table_MySQL.sql

--- creating_database_and_table_MySQL.sql
+++ creating_database_and_table_MySQL.sql
@@ -1,17 +1,17 @@
-show databases;
+SHOW DATABASE;
 CREATE DATABASE twitter;
-use twitter;
-show tables;
+USE twitter;
+SHOW tables;
 DROP TABLE IF EXISTS tweets;
 
 CREATE TABLE IF NOT EXISTS tweets (
 `tweet_id` BIGINT(20) NOT NULL PRIMARY KEY,
 `user_id` VARCHAR(25) DEFAULT NULL,
-`location` text DEFAULT NULL,
+`location` TEXT DEFAULT NULL,
 `user_followers_count` int(10) unsigned DEFAULT NULL,
-`date` date DEFAULT NULL,
+`date` DATE DEFAULT NULL,
 `hour` TINYINT(2) DEFAULT NULL,
-`tweet_text` text NOT NULL,
+`tweet_text` TEXT NOT NULL,
 `text_len`	SMALLINT(3) UNSIGNED NOT NULL,
 `top_1` VARCHAR(5) DEFAULT NULL,
 `top_2`	VARCHAR(5) DEFAULT NULL,
@@ -19,12 +19,12 @@ CREATE TABLE IF NOT EXISTS tweets (
 `polarity` TINYINT(1) DEFAULT NULL,
 `subjectivity` TINYINT(2) DEFAULT NULL,
 
-#`time` datetime DEFAULT NULL,
+#`time` DATETIME DEFAULT NULL,
 `is_retweet` BOOLEAN DEFAULT NULL,
 `ori_tweet_id` VARCHAR(25) DEFAULT NULL,
 `ori_user_id` VARCHAR(25) DEFAULT NULL,
-`favorite_count` int(10) unsigned DEFAULT NULL,
-`retweet_count` int(10) unsigned DEFAULT NULL,
+`favorite_count` int(10) UNSIGNED DEFAULT NULL,
+`retweet_count` int(10) UNSIGNED DEFAULT NULL,
 `is_quoted` BOOLEAN DEFAULT NULL,
 `quoted_id` VARCHAR(25) DEFAULT NULL,
 `quoted_author_id` VARCHAR(25) DEFAULT NULL,
