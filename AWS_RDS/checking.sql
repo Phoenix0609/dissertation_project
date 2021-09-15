c2d4d9e3f5914302e61351da4b2812f5ee3bc5a6
Fengxuan Zhang <41898293+Phoenix0609@users.noreply.github.com>
2021-09-15 22:15:38 +0100

    Update checking.sql

--- AWS_RDS/checking.sql
+++ AWS_RDS/checking.sql
@@ -2,4 +2,3 @@
 USE twitter;
 SELECT * FROM twitter.tweets;
 SELECT * FROM twitter.tweets WHERE `date` = '2021-08-20' AND `hour`= 15;
-SELECT * FROM twitter.tweets WHERE `tweet_id` = 1428737936216887296;
